9de728ae497750110f04e45b44e0c63c506c93a3
abhishekPhukan <abhishek.phukan91@gmail.com>
2023-05-29 11:01:25 +0530

    Release second version

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "image_id" {
   type = string
   description = "AMI ID to be used to create the instance"
 }
+
+variable "instance_name" {
+  type = string
+  description = "Name of the instance that would be created"
+}
